2b1dc68425ac903acc6d80d11b5e25d5c40bab92
Nathan Phillip Brink <ohnobinki@ohnopublishing.net>
2016-07-30 23:17:59 -0400

    Add .nuspec, nuget-pack script, and some publish guidelines.
    Hopefully these changes should make publishing to nuget
    straighforward.

--- SharpFileSystem.SharpZipLib/Properties/AssemblyInfo.cs
+++ SharpFileSystem.SharpZipLib/Properties/AssemblyInfo.cs
@@ -1,21 +1,23 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SharpFileSystem.SharpZipLib")]
-[assembly: AssemblyDescription("")]
+// NuGet automatically substitutes this as $description$ in the .nuspec
+[assembly: AssemblyDescription("Access a ZIP as a virtual file system")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+// NuGet automatically substitutes this as $author$ in the .nuspec
+[assembly: AssemblyCompany("Bob van der Linden")]
 [assembly: AssemblyProduct("SharpFileSystem.SharpZipLib")]
 [assembly: AssemblyCopyright("Copyright ©  2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
@@ -25,12 +27,18 @@ using System.Runtime.InteropServices;
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+// This is substituted as $version$ in the .nuspec. It lets you use free-form text,
+// meaning you can put semver (MAJOR.MINOR.PATCH) (3 parts rather than 4) in there.
+// This is also helpful because it shows up as “Product version” in Windows Explorer’s
+// Properties window.
+[assembly: AssemblyInformationalVersion("1.0.0")]
