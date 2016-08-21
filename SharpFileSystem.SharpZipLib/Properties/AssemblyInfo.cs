79fbaf5b39d7a688139b027eb26e7fed44f2a7e8
Bob van der Linden <bobvanderlinden@gmail.com>
2016-08-21 13:32:42 +0200

    Added appveyor configuration for artifacts (#13)
    * Added appveyor configuration for artifacts
    
    * Enabled "Package NuGet projects" in appveyor.yml
    
    * Added appveyor incrementing version
    
    * Added assembly info patching to appveyor configuration
    
    * Include nuget symbols
    
    * Use GlobalAssemblyInfo for all projects

--- SharpFileSystem.SharpZipLib/Properties/AssemblyInfo.cs
+++ SharpFileSystem.SharpZipLib/Properties/AssemblyInfo.cs
@@ -8,13 +8,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("SharpFileSystem.SharpZipLib")]
 // NuGet automatically substitutes this as $description$ in the .nuspec
 [assembly: AssemblyDescription("Access a ZIP as a virtual file system")]
-[assembly: AssemblyConfiguration("")]
-// NuGet automatically substitutes this as $author$ in the .nuspec
-[assembly: AssemblyCompany("Bob van der Linden")]
-[assembly: AssemblyProduct("SharpFileSystem.SharpZipLib")]
-[assembly: AssemblyCopyright("Copyright ©  2010")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
@@ -23,22 +16,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("b1df590b-1480-4e3d-baed-c6aebecc7187")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-
-// This is substituted as $version$ in the .nuspec. It lets you use free-form text,
-// meaning you can put semver (MAJOR.MINOR.PATCH) (3 parts rather than 4) in there.
-// This is also helpful because it shows up as “Product version” in Windows Explorer’s
-// Properties window.
-[assembly: AssemblyInformationalVersion("1.0.0")]
--- SharpFileSystem/Properties/AssemblyInfo.cs
+++ SharpFileSystem/Properties/AssemblyInfo.cs
@@ -8,13 +8,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("SharpFileSystem")]
 // NuGet automatically substitutes this as $description$ in the .nuspec
 [assembly: AssemblyDescription("A virtual file system for .NET written in C#")]
-[assembly: AssemblyConfiguration("")]
-// NuGet automatically substitutes this as $author$ in the .nuspec
-[assembly: AssemblyCompany("Bob van der Linden")]
-[assembly: AssemblyProduct("SharpFileSystem")]
-[assembly: AssemblyCopyright("Copyright ©  2010")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
@@ -23,24 +16,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("4b8cb295-69c1-40db-a9a8-03a2549b8f3d")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-
-// Never change this. It forces people to change assembly references and mostly just problems.
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-
-// This is substituted as $version$ in the .nuspec. It lets you use free-form text,
-// meaning you can put semver (MAJOR.MINOR.PATCH) (3 parts rather than 4) in there.
-// This is also helpful because it shows up as “Product version” in Windows Explorer’s
-// Properties window.
-[assembly: AssemblyInformationalVersion("1.0.0")]
