b9803a20c8f18215718ae4be6d047df7f2d14b1a
iason leiloglou <leiloglou@gmail.com>
2022-07-14 21:46:01 -0500

    added a file system based on the .NET ZipArchive

---
+++ SharpCoreFileSystem/FileSystems/NetZipArchiveFileSystem.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.IO.Compression;
+
+namespace SharpFileSystem.FileSystems
+{
+    public class NetZipArchiveFileSystem : IFileSystem
+    {
+        public ZipArchive ZipArchive { get; private set; }
+
+        public bool IsReadOnly => false;
+
+        public static NetZipArchiveFileSystem Open(Stream s)
+        { 
+            return new NetZipArchiveFileSystem(new ZipArchive(s,ZipArchiveMode.Update,true));
+        }
+
+        public static NetZipArchiveFileSystem Create(Stream s)
+        {
+            return new NetZipArchiveFileSystem(new ZipArchive(s, ZipArchiveMode.Create, true));
+        }
+
+        private NetZipArchiveFileSystem(ZipArchive archive)
+        {
+            ZipArchive = archive;
+        }
+        public void Dispose()
+        {
+            ZipArchive.Dispose();
+        }
+
+        protected IEnumerable<ZipArchiveEntry> GetZipEntries()
+        {
+            return ZipArchive.Entries;
+        }
+        protected FileSystemPath ToPath(ZipArchiveEntry entry)
+        {
+            return FileSystemPath.Parse(FileSystemPath.DirectorySeparator + entry.FullName);
+        }
+        protected string ToEntryPath(FileSystemPath path)
+        {
+            // Remove heading '/' from path.
+            return path.Path.TrimStart(FileSystemPath.DirectorySeparator);
+        }
+
+        protected ZipArchiveEntry ToEntry(FileSystemPath path)
+        {
+            return ZipArchive.GetEntry(ToEntryPath(path));
+        }
+        public ICollection<FileSystemPath> GetEntities(FileSystemPath path)
+        {
+            return GetZipEntries().Select(ToPath).Where(path.IsParentOf)
+                .Select(entryPath => entryPath.ParentPath == path
+                   ? entryPath
+                   : path.AppendDirectory(entryPath.RemoveParent(path).GetDirectorySegments()[0])
+                    )
+                .Distinct()
+                .ToList();
+        }
+
+        public bool Exists(FileSystemPath path)
+        {
+            if (path.IsFile)
+                return ToEntry(path) != null;
+            return GetZipEntries()
+                .Select(ToPath)
+                .Any(entryPath => entryPath.IsChildOf(path) || entryPath.Equals(path));
+        }
+
+        public Stream CreateFile(FileSystemPath path)
+        {
+            var zae = ZipArchive.CreateEntry(ToEntryPath(path));
+            return zae.Open();
+        }
+
+        public Stream OpenFile(FileSystemPath path, FileAccess access)
+        {
+            var zae = ZipArchive.GetEntry(ToEntryPath(path));
+            return zae.Open();
+        }
+
+        public void CreateDirectory(FileSystemPath path)
+        {
+            ZipArchive.CreateEntry(ToEntryPath(path));
+        }
+
+        public void Delete(FileSystemPath path)
+        {
+            var zae = ZipArchive.GetEntry(ToEntryPath(path));
+            zae.Delete();
+        }
+    }
+}
---
+++ SharpFileSystem.Tests/FileSystems/NetZipArchiveFileSystemTest.cs
@@ -0,0 +1,132 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using ICSharpCode.SharpZipLib.Zip;
+using SharpFileSystem.IO;
+using SharpFileSystem.FileSystems;
+using Xunit;
+
+namespace SharpFileSystem.Tests.FileSystems
+{
+    public class NetZipArchiveFileSystemTest
+    {
+        private Stream zipStream;
+        private NetZipArchiveFileSystem fileSystem;
+        private string fileContentString = "this is a file";
+
+        //setup 
+        public  NetZipArchiveFileSystemTest()
+        {
+            var memoryStream = new MemoryStream();
+            zipStream = memoryStream;
+            var zipOutput = new ZipOutputStream(zipStream);
+
+            
+            var fileContentBytes = Encoding.ASCII.GetBytes(fileContentString);
+            zipOutput.PutNextEntry(new ZipEntry("textfileA.txt")
+            {
+                Size = fileContentBytes.Length
+            });
+            zipOutput.Write(fileContentBytes);
+            zipOutput.PutNextEntry(new ZipEntry("directory/fileInDirectory.txt"));
+            zipOutput.PutNextEntry(new ZipEntry("scratchdirectory/scratch"));
+            zipOutput.Finish();
+
+            memoryStream.Position = 0;
+            fileSystem = NetZipArchiveFileSystem.Open(zipStream);
+        }
+
+        //teardown
+        public void Dispose()
+        {
+            fileSystem.Dispose();
+            zipStream.Dispose();
+        }
+
+        private readonly FileSystemPath directoryPath = FileSystemPath.Parse("/directory/");
+        private readonly FileSystemPath textfileAPath = FileSystemPath.Parse("/textfileA.txt");
+        private readonly FileSystemPath fileInDirectoryPath = FileSystemPath.Parse("/directory/fileInDirectory.txt");
+        private readonly FileSystemPath scratchDirectoryPath = FileSystemPath.Parse("/scratchdirectory/");
+
+        [Fact]
+        public void GetEntitiesOfRootTest()
+        {
+            Assert.Equal(new[]
+            {
+                textfileAPath,
+                directoryPath,
+                scratchDirectoryPath
+            }, fileSystem.GetEntities(FileSystemPath.Root).ToArray());
+        }
+
+        [Fact]
+        public void GetEntitiesOfDirectoryTest()
+        {
+            Assert.Equal(new[]
+            {
+                fileInDirectoryPath
+            }, fileSystem.GetEntities(directoryPath).ToArray());
+        }
+
+        [Fact]
+        public void ExistsTest()
+        {
+            Assert.True(fileSystem.Exists(FileSystemPath.Root));
+            Assert.True(fileSystem.Exists(textfileAPath));
+            Assert.True(fileSystem.Exists(directoryPath));
+            Assert.True(fileSystem.Exists(fileInDirectoryPath));
+            Assert.False(fileSystem.Exists(FileSystemPath.Parse("/nonExistingFile")));
+            Assert.False(fileSystem.Exists(FileSystemPath.Parse("/nonExistingDirectory/")));
+            Assert.False(fileSystem.Exists(FileSystemPath.Parse("/directory/nonExistingFileInDirectory")));
+        }
+
+        [Fact]
+        public void CanReadFile()
+        {
+            var file = fileSystem.OpenFile(textfileAPath, FileAccess.ReadWrite);
+            var text = file.ReadAllText();
+            Assert.True(string.Equals(text, fileContentString));
+        }
+
+        [Fact]
+        public void CanWriteFile()
+        {
+            var file = fileSystem.OpenFile(textfileAPath, FileAccess.ReadWrite);
+            var textBytes = Encoding.ASCII.GetBytes(fileContentString + " and a new string");
+            file.Write(textBytes);
+            file.Close();
+
+
+            file = fileSystem.OpenFile(textfileAPath, FileAccess.ReadWrite);
+            var text = file.ReadAllText();
+            Assert.True(string.Equals(text, fileContentString + " and a new string"));
+        }
+
+        [Fact]
+        public void CanAddFile()
+        {
+            var fsp = FileSystemPath.Parse("/scratchdirectory/recentlyadded.txt");
+            var file = fileSystem.CreateFile(fsp);
+            var textBytes = Encoding.ASCII.GetBytes("recently added");
+            file.Write(textBytes);
+            file.Close();
+
+            Assert.True(fileSystem.Exists(fsp));
+
+            file = fileSystem.OpenFile(fsp, FileAccess.ReadWrite);
+            var text = file.ReadAllText();
+            Assert.True(string.Equals(text, "recently added"));
+        }
+
+        [Fact]
+        public void CanAddDirectory()
+        {
+            var fsp = FileSystemPath.Parse("/scratchdirectory/dir/");
+            fileSystem.CreateDirectory(fsp);
+
+            Assert.True(fileSystem.Exists(fsp));
+        }
+    }
+}
